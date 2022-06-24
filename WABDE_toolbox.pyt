0bfc043dc9bf832f048e7884a8312a01132f34c5
brian32768 <brian@wildsong.biz>
2022-06-24 10:16:19 -0700

    building toolbox

---
+++ WABDE_toolbox.pyt
@@ -0,0 +1,52 @@
+"""
+WebAppBuilder Python Toolbox Template (a ".pyt" file)
+
+@author: Brian Wilson <brian@wildsong.biz>
+"""
+from __future__ import print_function
+import arcpy
+
+# This is for development, so that you can edit code while running in ArcGIS Pro.
+import importlib
+import field_update_tool
+importlib.reload(field_update_tool)
+
+# Import all the tool classes that will be included in this toolbox.
+from field_update_tool import Field_Update_tool
+#from python_tool_template import Sample_Tool_2
+
+class Toolbox(object):
+    def __init__(self):
+        """Define the toolbox (the name of the toolbox is the name of this .pyt file)."""
+        self.description = """Sample toolbox containing sample tools."""
+
+        self.label = "My Python Toolbox"
+        self.alias = "SampleToolbox"  # no special characters including spaces!
+        self.description = """My toolbox containing python tools!"""
+
+        # List of tool classes associated with this toolbox
+        self.tools = [
+            Field_Update_tool,
+            #Sample_Tool_2
+        ]
+
+def list_tools():
+    toolbox = Toolbox()
+    print("toolbox:", toolbox.label)
+    print("description:", toolbox.description)
+    print("tools:")
+    for t in toolbox.tools:
+        tool = t()
+        print('  ', tool.label)
+        print('   description:', tool.description)
+        for param in tool.getParameterInfo():
+            print('    ',param.name,':',param.displayName)
+        print()
+
+
+if __name__ == "__main__":
+    # Running this as a standalone script lists information about the toolbox and each tool.
+    list_tools()
+    #exit(0) # This causes the toolbox not to load in ArcGIS Pro. Whatever.
+
+# That's all!
--- remove_icon.py
+++ remove_icon.py
@@ -1,17 +1,30 @@
 """
  Read a config.json file and remove the weird references to icon.png files.
 
+ Normally I expect this to be called from a Python Toolbox but you can use it at the command line too.
+
  Give me a filename and I will repair the file and spit the new data out to STDOUT.
  Give me nothing and I will run a unit test
+
+ Use these commands to test it on sample.json; look at results, and restore original sample.json
+
+   python remove_icon.py sample.json
+   diff sample.json*
+   mv sample.json.* sample.json
 """
 import sys
 import re
 from datetime import datetime
 
-def do_work(data):
+def do_repair_work(data) -> list:
+    """
+    Process a list with the data from the file in it,
+    
+    Return a list of repaired data.
+    """
     previous_line = None
     re_search = re.compile(r'"icon":\s+\"/webappbuilder')
-
+    output = list()
     for line in data:
         line = line.rstrip() # Remove newline
         if previous_line:
@@ -20,10 +33,38 @@ def do_work(data):
                 if previous_line.endswith(','):
                     previous_line = previous_line[:-1]
                 line = None # this line will be dropped
-            print(previous_line)
+            output.append(previous_line + '\n')
         previous_line = line
     if previous_line:
-        print(previous_line)
+        output.append(previous_line + '\n')
+    return output
+
+
+def remove_icon(config_file: str) -> str:
+    """
+    Removes widget icons from the JSON file after making a backup of it.
+
+    Returns the name of the backup file.
+    """
+    datestamp = datetime.now().strftime("%Y%m%d-%H%M")  # good for filenames
+
+    # Read the original file into memory
+    with open(config_file, "r") as fp:
+        config_data = fp.readlines()
+
+    # Write the original data into a backup file
+    backup_file = config_file + '.' + datestamp
+    with open(backup_file, "w") as fp:
+        fp.writelines(config_data)
+    print("Wrote backup file \"%s\"" % backup_file)
+
+    repaired_data = do_repair_work(config_data)
+
+    with open(config_file, "w") as fp:
+        fp.writelines(repaired_data)
+
+    return backup_file
+
 
 def usage() -> None:
     print("""
@@ -35,6 +76,7 @@ Filters out the stupid "icon" lines from WABDE config.json files.
 The backup copy of the file will have a datestamp, like "config.json.20220624"
 """)
     exit(-1)
+
 
 if __name__ == "__main__":
 
@@ -50,17 +92,7 @@ if __name__ == "__main__":
             "5 This should be the last line."
         ]
         print("Running tests")
-        do_work(test_data)
+        do_repair_work(test_data)
         exit(0)
 
-    datestamp = datetime.now().strftime("%Y%m%d-%H%M") # good for filenames
-    config = sys.argv[1]
-    with open(config, "r") as fp:
-        config_data = fp.readlines()
-
-    backup = config + '.' + datestamp
-    with open(backup, "w") as fp:
-        fp.writelines(backup)
-    print("Wrote backup file \"%s\"" % backup)
-    do_work(config_data)
-
+    remove_icon(sys.argv[1])
